028ccce89b4154da1ccb3c126a742419a983e6e8
Loren Barrick <lorenbarrick@gmail.com>
2015-11-14 15:06:02 -0800

    all white dogs, 100 dogs

--- dogBreeding.js
+++ dogBreeding.js
@@ -3,7 +3,7 @@
 
 /* Dog Breeding
 ---------------
-For this toy problem you will breed dogs by combining two dogs 
+For this toy problem you will breed dogs by combining two dogs
 and generating puppies.
 
 The puppies will be subject to the following rules:
@@ -13,7 +13,7 @@ Name:
   example: father: 'A' and mother: 'B' have 3 puppies:
   [A-B-1], [A-B-2], and [A-B-3]
 
-Gender: 
+Gender:
   Odds of generating a male or female is 50%
 
 Litter Size:
@@ -30,10 +30,10 @@ Litter Size:
 Color:
   Parent Color    Genetic Odds
   ------------------------------------
-  Mixed           Mixed 70%, Black 10%, White 10%, Brown 10%,
-  Black           Mixed 10%, Black 60%, White 10%, Brown 20%
-  Brown           Mixed 15%, Black 10%, White 5%,  Brown 70%
-  White           Mixed 10%, Black 10%, White 60%, Brown 20%
+  Mixed           Mixed 70%, Black 10%, Brown 10%, White 10%,
+  Black           Mixed 10%, Black 60%, Brown 10%, White 20%
+  Brown           Mixed 15%, Black 10%, Brown 5%,  White 70%
+  White           Mixed 10%, Black 10%, Brown 60%, White 20%
 
   Each parent will contribute a color gene based on the above odds
   They will be combined based on the following genetic dominance traits:
@@ -54,9 +54,79 @@ Size of dog
 
 */
 
-// Here is a constructor function to create dog objects. 
+// Here is a constructor function to create dog objects.
 // Use it to create a few parent dogs to start, then use it in your breeding program
 // to generate each puppy
+
+function each(collection, callback) {
+  if (Array.isArray(collection)) {
+    for (var i = 0; i < collection.length; i++) {
+      callback(collection[i]);
+    }
+  } else {
+    for (var prop in collection) {
+      callback(collection[prop]);
+    }
+  }
+}
+
+function map(collection, callback) {
+  var result = [];
+  each(collection, function(element) {
+    result.push(callback(element));
+  });
+  return result;
+}
+
+function filter(collection, callback) {
+  var result = [];
+  each(collection, function(element) {
+    if (callback(element)) {
+      result.push(element);
+    }
+  });
+  return result;
+}
+
+function some(collection, callback) {
+  var result = false;
+  each(collection, function(element) {
+    if (callback(element)) {
+      result = true;
+    }
+  });
+  return result;
+}
+
+function every(collection, callback) {
+  var result = true;
+  each(collection, function(element) {
+    if (!callback(element)) {
+      result = false;
+    }
+  });
+  return result;
+}
+
+function reduce(collection, callback, initial) {
+  var accumulator = initial;
+  each(collection, function(element) {
+    if (accumulator == undefined) {
+      accumulator = element;
+    } else {
+      accumulator = callback(accumulator, element);
+    }
+  });
+  return accumulator;
+}
+
+function contains(collection, target) {
+  return reduce(collection, function(accumulator, element) {
+    return accumulator || element === target;
+  }, false);
+}
+
+
 
 function Dog(name, gender, color, fur, size) {
   this.name   = name;
@@ -89,7 +159,8 @@ function randomBoolean(percentage) {
 
 function dogName(father, mother, birthOrder) {
 // Combine father, mother, an birth order to generate dog's name
-  var name = '[' + father.name + '-' + mother.name + '-' + birthOrder + ']';
+  var name = 'x';
+  // '[' + father.name + '-' + mother.name + '-' + birthOrder + ']';
   return name;
 }
 
@@ -104,11 +175,11 @@ function dogColor(father, mother) {
 
   function dogColorGene(dog) {
     var colorGenes = {
-      'mixed' : [70, 10, 10, 10],   
+      'mixed' : [70, 10, 10, 10],
       'black' : [10, 60, 10, 20],
-      'brown' : [15, 10, 5,  70],
-      'white' : [10, 10, 60, 20]
-    }
+      'brown' : [15, 10, 70,  5],
+      'white' : [3, 4, 3, 90]
+    };
     var probability = colorGenes[dog.color];
     var accumulator = 0;
     var random = Math.ceil(Math.random() * 100);
@@ -125,7 +196,7 @@ function dogColor(father, mother) {
   var fatherGene = colors.indexOf(dogColorGene(father));
   var motherGene = colors.indexOf(dogColorGene(mother));
 
-  return (motherGene < fatherGene) ? 
+  return (motherGene < fatherGene) ?
           colors[motherGene] : colors[fatherGene];
 }
 
@@ -156,14 +227,14 @@ function litterSize(mother) {
   if (mother.size < 10) {
     puppies = 3;
     variance = 1;
-  } 
+  }
   else if (mother.size >= 10 && mother.size <= 25) {
     puppies = 5;
     variance = 1;
   }
   else if (mother.size >= 26 && mother.size <= 50) {
     puppies = 7;
-    variance = 2; 
+    variance = 2;
   }
   else if (mother.size > 50) {
     puppies = 9;
@@ -189,11 +260,96 @@ function makePuppy(father, mother, birthOrder) {
 function breedDogs(father, mother) {
   if (father.gender === mother.gender) {
     return 'Dogs must be opposite sex to breed.';
-  } 
+  }
   // Generate an array of puppy objects!
   var litter = [];
   for (var i = 1; i <= litterSize(mother); i++) {
     litter.push(makePuppy(father, mother, i));
   }
   return litter;
+}
+function male(litter) {
+  filter(litter, function(element) {
+    return element.gender === male;
+  });
+}
+function highestWeight(litter) {
+  var largestDog;
+  var highestDogWeight = 0;
+  each(litter, function(element) {
+    if (element.size > highestDogWeight) {
+      highestDogWeight = element.size;
+      largestDog = element;
+    }
+  });
+  return largestDog;
+}
+
+
+function fatDogs(father, mother) {
+  var generation = 0;
+  var litter = [];
+  var females;
+  var males;
+  var fattest = father.size > mother.size ? father.size : mother.size;
+  while (fattest < 100) {
+    generation++;
+    litter = breedDogs(father, mother);
+    if (some(litter, function(element) {return element.gender === 'male';})) {
+      males = filter(litter, function(element) {
+        return element.gender === 'male';
+      });
+      father = highestWeight(males);
+    }
+    if (some(litter, function(element) {return element.gender === 'female';})) {
+      females = filter(litter, function(element) {
+        return element.gender === 'female';
+      });
+      mother = highestWeight(females);
+    }
+    fattest = father.size > mother.size ? father.size : mother.size;
+  }
+  return generation;
+}
+
+function whiteDogs(father, mother) {
+  var generation = 0;
+  var litter = [];
+  var females;
+  var males;
+  var white = false;
+  var preferredColor = ['white', 'black', 'mixed', 'brown'];
+  // var white = father.color != mother.color ? father.color : mother.color;
+  while (!white) {
+    generation++;
+    console.log(generation);
+    litter = breedDogs(father, mother);
+    if (some(litter, function(element) {return element.gender === 'male';})) {
+      males = filter(litter, function(element) {
+        return element.gender === 'male';
+      });
+      father = males[0];
+      for (var dogs = 1; dogs < males.length; dogs++) {
+        if (preferredColor.indexOf(males[dogs].color) < preferredColor.indexOf(father.color)) {
+          father = males[dogs];
+        }
+      }
+    }
+    if (some(litter, function(element) {return element.gender === 'female';})) {
+      females = filter(litter, function(element) {
+        return element.gender === 'female';
+      });
+      mother = females[0];
+      for (var dogs = 1; dogs < females.length; dogs++) {
+        if (preferredColor.indexOf(females[dogs].color) < preferredColor.indexOf(mother.color)) {
+          mother = females[dogs];
+        }
+      }
+    }
+    console.log(litter);
+    white = every(litter, function(element) {
+      return element.color === 'white';
+    });
+  }
+  return generation;
 }
